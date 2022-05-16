f91cdd0b85fb799dfd22470fca312faa5fc1207a
Marlon Regis Amaral Pinto da Silva <mr.silva@samsung.com>
2022-05-16 15:29:20 -0300

    Add project files.

---
+++ PhotoOrganize/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace PhotoOrganize
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+    }
+}
